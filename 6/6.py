997f952fcbbffbb34a0e7a1d71fca515a0ac955d
zachmanson <68168088+zachmanson@users.noreply.github.com>
2022-12-06 00:18:27 -0500

    Add files via upload

---
+++ 6/6.py
@@ -0,0 +1,15 @@
+with open('input6.txt') as reader:
+    text = reader.readline().strip()
+    #window_size = 4 #part one
+    window_size = 14 #part two
+    for i in range(len(text) - window_size + 1):
+        window = text[i: i + window_size]
+        if len(window) == len(set(window)):
+            print(i + window_size)
+            break
+
+
+            
+
+
+
